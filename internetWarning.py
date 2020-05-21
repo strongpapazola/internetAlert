a2212d5cf0d36944e16af3339b2abd7a94d80f4f
strongpapazola <48545591+strongpapazola@users.noreply.github.com>
2020-05-21 08:05:30 +0700

    Add files via upload

---
+++ internetWarning.py
@@ -0,0 +1,20 @@
+from urllib.request import urlopen
+import time
+from os import system
+
+def internet_on():
+ try:
+  urlopen('https://www.google.com', timeout=1)
+  return True
+ except:
+  return False
+
+while True:
+ time.sleep(1)
+ data = ''
+ if internet_on() == False:
+  data = 'true'
+ else:
+  data = ''
+ if data == 'true':
+  system('play -q error.mp3 &')
